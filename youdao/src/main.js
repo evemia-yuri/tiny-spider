659cd5e5647880beab7e08e330a0cd9b256d3c9f
evemia-yuri <suimiaomiaomiao@163.com>
2025-11-04 23:21:27 +0800

    Revert "Tweaked the code logic"
    This reverts commit 0a7b7ec5880c293b70fe95d1e6166a608a399067.

--- youdao/src/main.js
+++ youdao/src/main.js
@@ -1,51 +1,55 @@
 import axios from 'axios'
-import {writeFile} from 'fs/promises';
 import crypto from 'node:crypto';
 import {stringify} from 'node:querystring';
 
 /**
- * Generate request payload data
- * @param {string} query target words or sentence
- * @param {string} target target translate language
- * @returns {string} payload string
+ * 构造请求负载数据
+ * @param {string} 目标查询单词或者句子
+ * @param {string} 目标翻译语言
+ * @returns {string} 构造好的负载数据字符串
  */
-function buildPayload(query, lang) {
+function buildPayload(query, targetLang) {
   const hFunc = (token) => {
     return crypto.createHash('md5').update(token.toString()).digest('hex')
   };
-  const keyform = 'webdict';
-  const time = ''.concat(query).concat(keyform).length % 10;
-  const secret = 'Mk6hqtUp33DGGtoS63tTJbMUYjRrG1Lu';
+  let form = 'webdict';
+  let time = ''.concat(query).concat(form).length % 10;
 
-  let r = ''.concat(query).concat(keyform);
+  let r = ''.concat(query).concat(form);
   let o = hFunc(r);
-  let n = ''.concat('web').concat(query).concat(time).concat(secret).concat(o);
-  let sign = hFunc(n);
+  let n = ''.concat('web')
+              .concat(query)
+              .concat(time)
+              .concat('Mk6hqtUp33DGGtoS63tTJbMUYjRrG1Lu')
+              .concat(o)
+  let sig = hFunc(n);
 
   return stringify({
     q: query,
-    le: lang,
+    le: targetLang,
     t: time,
     client: 'web',
-    sign: sign,
-    keyform: keyform
+    sign: sig,
+    keyform: form
   });
 }
 
-async function main() {
+async function query() {
   const headers = {
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
-         AppleWebKit/537.36 (KHTML, like Gecko) \
-         Chrome/142.0.0.0 Safari/537.36 Edg/142.0.0.0',
+    'user-agent':
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36 Edg/142.0.0.0',
     'content-type': 'application/x-www-form-urlencoded;charset=utf-8'
   };
-  let payload = buildPayload('word', 'en');
+  let payload = buildPayload('我的世界', 'en');
   const response = await axios.post(
       'https://dict.youdao.com/jsonapi_s?doctype=json&jsonversion=4', payload,
       {headers: headers});
 
-  const fileName = 'output.json';
-  await writeFile(fileName, JSON.stringify(response.data, null, 2));
+  console.log(response.data);
 }
 
+function main() {
+  console.log('Hello Youdao!');
+  query()
+}
 main();
