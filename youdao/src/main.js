415c6c0df6fd0c8e89bb4e8cd344b97b71219739
evemia-yuri <suimiaomiaomiao@163.com>
2025-11-05 15:24:30 +0800

    feat: The crawler module has been improved, achieving data extraction and cleaning.

--- youdao/src/main.js
+++ youdao/src/main.js
@@ -3,27 +3,36 @@ import {writeFile} from 'fs/promises';
 import crypto from 'node:crypto';
 import {stringify} from 'node:querystring';
 
+const HEADERS = {
+  'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
+         AppleWebKit/537.36 (KHTML, like Gecko) \
+         Chrome/142.0.0.0 Safari/537.36 Edg/142.0.0.0',
+  'content-type': 'application/x-www-form-urlencoded;charset=utf-8'
+};
+const BASE_URL = 'https://dict.youdao.com/jsonapi_s?doctype=json&jsonversion=4';
+
 /**
- * Generate request payload data
- * @param {string} query target words or sentence
- * @param {string} target target translate language
- * @returns {string} payload string
+ * Generate request payload data.
+ * @param {string} keyword Target words or sentence.
+ * @param {string} target Target translate language.
+ * @returns {string} Payload string.
  */
-function buildPayload(query, lang) {
+function buildPayload(keyword, lang) {
   const hFunc = (token) => {
     return crypto.createHash('md5').update(token.toString()).digest('hex')
   };
   const keyform = 'webdict';
-  const time = ''.concat(query).concat(keyform).length % 10;
+  const time = ''.concat(keyword).concat(keyform).length % 10;
   const secret = 'Mk6hqtUp33DGGtoS63tTJbMUYjRrG1Lu';
 
-  let r = ''.concat(query).concat(keyform);
-  let o = hFunc(r);
-  let n = ''.concat('web').concat(query).concat(time).concat(secret).concat(o);
-  let sign = hFunc(n);
+  const r = ''.concat(keyword).concat(keyform);
+  const o = hFunc(r);
+  const n =
+      ''.concat('web').concat(keyword).concat(time).concat(secret).concat(o);
+  const sign = hFunc(n);
 
   return stringify({
-    q: query,
+    q: keyword,
     le: lang,
     t: time,
     client: 'web',
@@ -32,20 +41,68 @@ function buildPayload(query, lang) {
   });
 }
 
+/**
+ * Make a request to the server and return the original json data.
+ * @param {string} keyword Target words or sentence.
+ * @param {string} target Target translate language.
+ * @returns {Promise<object>} Raw json data from server which contains words and
+ *     sentences.
+ */
+async function query(keyword, lang) {
+  const wordsPromise =
+      axios.post(BASE_URL, buildPayload(keyword, lang), {headers: HEADERS});
+  const sentencesPromise = axios.post(
+      BASE_URL, buildPayload('lj:'.concat(keyword), lang), {headers: HEADERS});
+
+  try {
+    const [wordsReponse, sentencesResponse] =
+        await Promise.all([wordsPromise, sentencesPromise]);
+    return {words: wordsReponse.data, sentences: sentencesResponse.data};
+  } catch (error) {
+    console.log(`Error from function 'query()': ${error}`);
+    return {};
+  }
+}
+
+/**
+ * Extract the target data from raw json data.
+ * @param {object} rawData Raw json data from server.
+ * @returns {object} Target data.
+ */
+function dataExtraction(rawData) {
+  /** @type {Array<{'#text': string, '#tran': string}>} */
+  const wordsRaw = rawData.words.ce.word.trs;
+  const words = wordsRaw.reduce((acc, word) => {
+    acc[word['#text']] = word['#tran'];
+    return acc;
+  }, {});
+
+  /** @type {Array<{key: string, trans: Array<{'value': string}>}>} */
+  const phrasesRaw = rawData.words.web_trans['web-translation'];
+  const phrases = phrasesRaw.slice(1).reduce((acc, phrase) => {
+    acc[phrase.trans[0].value] = phrase.key;
+    return acc;
+  }, {});
+
+  /** @type {Array<{sentence: string, 'sentence-translation': string}>} */
+  const sentencesRaw = rawData.sentences.blng_sents['sentence-pair'];
+  const sentences = sentencesRaw.reduce((acc, sentence) => {
+    acc[sentence['sentence-translation']] = sentence.sentence;
+    return acc;
+  }, {});
+
+  return {words, phrases, sentences};
+}
+
+/**
+ * Entry of this project.
+ */
 async function main() {
-  const headers = {
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
-         AppleWebKit/537.36 (KHTML, like Gecko) \
-         Chrome/142.0.0.0 Safari/537.36 Edg/142.0.0.0',
-    'content-type': 'application/x-www-form-urlencoded;charset=utf-8'
-  };
-  let payload = buildPayload('word', 'en');
-  const response = await axios.post(
-      'https://dict.youdao.com/jsonapi_s?doctype=json&jsonversion=4', payload,
-      {headers: headers});
+  const responseData = await query('火光', 'en');
 
   const fileName = 'output.json';
-  await writeFile(fileName, JSON.stringify(response.data, null, 2));
+  await writeFile(
+      fileName, JSON.stringify(dataExtraction(responseData), null, 2));
 }
 
 main();
